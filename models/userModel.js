e8207f5615320fd699eb8c0c854e5b133d09bf77
Ariana Garcia <ariana_garcia@ymail.com>
2019-04-01 22:14:05 -0500

    added queries

--- models/userModel.js
+++ models/userModel.js
@@ -36,7 +36,7 @@ retrieveUser('blake','password')
 
 /**
  * insert user into the DB.
- */
+ *
 const createUser = function(connection, user_id, name, password, email){
   connection.query('INSERT INTO users (user_id, name, password, email) VALUES ($1, $2, $3, $4)', [user_id, name, password, email], (error, results) => {
     if (error) {
@@ -45,10 +45,78 @@ const createUser = function(connection, user_id, name, password, email){
     console.log(results.rows)
     return results.rows.length
 });
-}
+}*/
 
+const createUser = async function(user_id, name, password, email) {
+    //Use connection after the db is set up to retreive user information from the DB.
+    //connection.query('SELECT user_id, name FROM users WHERE  email = $1 and password = crypt($2, password)', [email,password], (error, results) => {
+    return connection.task('createUser', function *(t) {
+          return t.none('INSERT INTO users (user_id, name, password, email) VALUES ($1, $2, $3, $4)', [user_id,name,password,email]);
+      });
+  }
+
+
+
+/**
+ * Retrieve student from the DB.
+ */
+const retrieveStudent = async function(user_id) {
+    //Use connection after the db is set up to retreive user information from the DB.
+    //connection.query('SELECT user_id, name FROM users WHERE  email = $1 and password = crypt($2, password)', [email,password], (error, results) => {
+    return connection.task('retrieveStudent', function *(t) {
+          const student = yield t.any('Select users.user_id, name, email From users, student where  users.user_id = $1', [user_id]);
+          return yield student;
+      });
+  }
+
+  retrieveStudent(1)
+  .then(student => {
+      console.log(student[0]);
+  })
+  .catch(error =>{
+      console.log("error");
+  });
+
+/**
+ * Retrieve instructor from the DB.
+ */
+const retrieveInstructor = async function(user_id) {
+    return connection.task('retrieveInstructor', function *(t) {
+          const instructor = yield t.any('Select users.user_id, name, email From users, instructor where  users.user_id = $1', [user_id]);
+          return yield instructor;
+      });
+  }
+
+  retrieveInstructor(13)
+  .then(instructor => {
+      console.log(instructor[0]);
+  })
+  .catch(error =>{
+      console.log("error");
+  });
+
+/**
+ * Retrieve GA from the DB.
+ */
+const retrieveGA = async function(user_id) {
+    return connection.task('retrieveGA', function *(t) {
+          const GA= yield t.any('Select users.user_id, name, email From users, ga where  users.user_id = $1', [user_id]);
+          return yield GA;
+      });
+  }
+
+  retrieveGA(20)
+  .then(ga => {
+      console.log(ga[0]);
+  })
+  .catch(error =>{
+      console.log("error");
+  });
 
 module.exports = {
     retrieveUser,
-    createUser
+    createUser,
+    retrieveStudent,
+    retrieveInstructor,
+    retrieveGA,
   }
