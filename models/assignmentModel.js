44355b9dba1a07ba269d6c134065c1c8fd2f5660
samuelreagan <samreagan96@gmail.com>
2019-04-07 15:56:23 -0500

    Removed yield and async from models

--- models/assignmentModel.js
+++ models/assignmentModel.js
@@ -3,10 +3,10 @@ var connection = require("./db.js");
 /**
  * Retrieve all courses assignments from the DB.
  */
-const retrieveCourses_assignments  = async function(course_id) {
+const retrieveCourses_assignments  = function(course_id) {
     return connection.task('retrieveCourses_assignments ', function *(t) {
-        const assignments= yield t.any('select * from assignments where assignment_id IN (select assignment_id from course_assignments where course_id = $1);', [course_id]);
-        return yield assignments;
+        const assignments = t.any('select * from assignments where assignment_id IN (select assignment_id from course_assignments where course_id = $1);', [course_id]);
+        return assignments;
     });
 }
 
--- models/noteModel.js
+++ models/noteModel.js
@@ -3,10 +3,10 @@ var connection = require("./db.js");
 /**
  * Retrieve all courses notes from the DB.
  */
-const retrieveCourses_notes  = async function(course_id) {
+const retrieveCourses_notes  =  function(course_id) {
     return connection.task('retrieveCourses_notes ', function *(t) {
-          const notes= yield t.any('select * from  notes where note_id IN (select note_id from course_notes where course_id = $1)', [course_id]);
-          return yield notes;
+          const notes = t.any('select * from  notes where note_id IN (select note_id from course_notes where course_id = $1)', [course_id]);
+          return notes;
       });
   }
 
