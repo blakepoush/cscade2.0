6ce6f6664560c0ae20fe37ed546f678d5d8cae58
ariana garcia <ariana_garcia@ymail.com>
2019-04-12 11:12:23 -0500

    added usefulLinksModel and announcementModel

---
+++ models/announcementModel.js
@@ -0,0 +1,16 @@
+var connection = require("./db.js");
+
+/**
+ * Retrieve all announcements from the DB.
+ */
+const retrieveAnnouncements  = function() {
+    return connection.task('retrieveAnnouncements ', function *(t) {
+        const announcements = t.any('select * from announcements;');
+        return announcements;
+    });
+}
+
+
+module.exports = {
+    retrieveAnnouncements
+  }
---
+++ models/usefulLinksModel.js
@@ -0,0 +1,16 @@
+var connection = require("./db.js");
+
+/**
+ * Retrieve all usefulLinks from the DB.
+ */
+const retrieveUsefulLinks  = function() {
+    return connection.task('retrieveUsefulLinks ', function *(t) {
+        const links = t.any('select * from useful_links;');
+        return links;
+    });
+}
+
+
+module.exports = {
+    retrieveUsefulLinks
+  }
