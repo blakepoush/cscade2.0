0033959777f1641b7ce23828dddaa382b9f62d4a
blakepoush <poushbla@gmail.com>
2019-04-19 21:16:56 -0500

    added courseGrades partial, changed grades model to return more data

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -6,7 +6,7 @@ var connection = require("./db.js");
 
 const retrieveGrades  =  function(user_id, course_id) {
     return connection.task('retrieveGrades ', function *(t) {
-          const grades = t.any('select assignments.assignment_id, assignments.title, course_assignment_types.type, grade from submitted_assignments, assignments, course_assignment_types where grade is not null and student_id = $1 and submitted_assignments.assignment_id = assignments.assignment_id and submitted_assignments.assignment_id IN (select course_assignments.assignment_id from course_assignments where course_id = $2) and assignments.type = course_assignment_types.type_id and course_assignment_types.course_id = $2;', [user_id, course_id]);
+          const grades = t.any('select assignments.assignment_id, assignments.title, course_assignment_types.type, grade, maxPoints, dueDate from submitted_assignments, assignments, course_assignment_types where grade is not null and student_id = $1 and submitted_assignments.assignment_id = assignments.assignment_id and submitted_assignments.assignment_id IN (select course_assignments.assignment_id from course_assignments where course_id = $2) and assignments.type = course_assignment_types.type_id and course_assignment_types.course_id = $2;', [user_id, course_id]);
           return grades;
       });
   }
