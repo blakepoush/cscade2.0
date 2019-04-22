24543713786c7f3344299950d99618c51791ce0a
samuelreagan <samreagan96@gmail.com>
2019-04-22 18:01:14 -0500

    Fixed time

--- public/scripts/servertime.js
+++ public/scripts/servertime.js
@@ -5,7 +5,10 @@ function servertime(){
     var hours = d.getHours() % 12;
     var time = "";
     time += (hours > 0) ? hours : d.getHours();
-    time += ":" + d.getMinutes() + ":" + d.getSeconds()
+    time += ":";
+    time +=  (d.getMinutes() > 9) ? d.getMinutes() : `0${d.getMinutes()}`;
+    time += ":";
+    time +=  (d.getSeconds() > 9) ? d.getSeconds() : `0${d.getSeconds()}`;
     time += (hours > 0) ? " PM" : " AM";
     document.getElementById("serverTime").textContent = date + " - " + time;
 }
