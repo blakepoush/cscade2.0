ac6f779e227850f679c9811ae149ecfb25a829e5
ariana garcia <ariana_garcia@ymail.com>
2019-04-22 23:24:57 -0500

    changed retrieve user past and current assignment functions

--- models/assignmentModel.js
+++ models/assignmentModel.js
@@ -25,10 +25,11 @@ const retrieveUserAssignments = function(user_id) {
  */
 const retrieveUserCurrentAssignments = function(user_id) {
     return connection.task('retrieveUserCurrentAssignments', function(t) {
-        const assignments = t.any('select * from assignments natural join course_assignments natural join enrolled natural join courses where student_id = $1 and dueDate >= now()', [user_id]);
+        const assignments = t.any('select * , isSubmitted($1, assignment_id) from assignments natural join course_assignments natural join enrolled natural join courses where student_id = $1 and dueDate >= now()', [user_id]);
         return assignments;
     });
 }
+
 
 
 /**
@@ -36,10 +37,12 @@ const retrieveUserCurrentAssignments = function(user_id) {
  */
 const retrieveUserPastAssignments = function(user_id) {
     return connection.task('retrieveUserPastAssignments', function(t) {
-        const assignments = t.any('select * from assignments natural join course_assignments natural join enrolled natural join courses where student_id = $1 and dueDate < now()', [user_id]);
+        const assignments = t.any('select * , isSubmitted($1, assignment_id) from assignments natural join course_assignments natural join enrolled natural join courses where student_id = $1 and dueDate < now()', [user_id]);
         return assignments;
     });
 }
+
+
 
 /**
  * Retrieve all of the user's current assignments for a course.
