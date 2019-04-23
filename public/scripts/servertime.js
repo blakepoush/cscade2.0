97800db5e43378fb639e161096049343e0fa47a8
samuelreagan <samreagan96@gmail.com>
2019-04-23 08:29:49 -0500

    Fixed servertime bug

--- public/scripts/servertime.js
+++ public/scripts/servertime.js
@@ -3,13 +3,14 @@ function servertime(){
     var d = new Date();
     var date = (d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear();
     var hours = d.getHours() % 12;
+    var isAmPm = d.getHours() / 12;
     var time = "";
     time += (hours > 0) ? hours : d.getHours();
     time += ":";
     time +=  (d.getMinutes() > 9) ? d.getMinutes() : `0${d.getMinutes()}`;
     time += ":";
     time +=  (d.getSeconds() > 9) ? d.getSeconds() : `0${d.getSeconds()}`;
-    time += (hours > 0) ? " PM" : " AM";
+    time += (isAmPm >= 1) ? " PM" : " AM";
     document.getElementById("serverTime").textContent = date + " - " + time;
 }
 
