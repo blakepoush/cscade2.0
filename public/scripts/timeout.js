704ed83918a96598f485a6a4b246de71570f0427
Hunter Ingle <44213633+hunteringle20@users.noreply.github.com>
2019-04-28 22:35:41 -0500

    Add files via upload

--- public/scripts/timeout.js
+++ public/scripts/timeout.js
@@ -1,11 +1,11 @@
 
 function idleTimer() {
-  var timoutWarning = 540000; // Display warning at 9 minutes
-	var timoutNow = 600000; // Timeout in 10 minutes
-	var logoutUrl = '/logout'; // URL to logout page.
+  var timoutWarning = 540000; //Warning at 9 minutes
+	var timoutNow = 600000; //Timeout at 10 minutes
+	var logoutUrl = '/logout'; //Route to logout when timed out
 	var body = document.getElementById("body");
 
-	// Add event listners for clicking the page or moving the mouse over the page
+	// Add event listeners for interacting with the page
 	body.addEventListener("click", ResetTimers);
 	body.addEventListener("mousemove", ResetTimers);
 	window.addEventListener("scroll", ResetTimers);
@@ -13,29 +13,27 @@ function idleTimer() {
 	var warningTimer;
 	var timeoutTimer;
 
-	// Start the Timers
 	StartTimers();
 
-	// Start Timers Method
+	//Start timer function
 	function StartTimers() {
 		warningTimer = setTimeout(IdleWarning, timoutWarning);
 		timeoutTimer = setTimeout(IdleTimeout, timoutNow);
 	}
 
-	// Reset timers.
+	//Reset timer function
 	function ResetTimers() {
-		console.log("reset");
 		clearTimeout(warningTimer);
 		clearTimeout(timeoutTimer);
 		StartTimers();
 	}
 
-	// Show idle timeout warning dialog.
+	//Idle warning message
 	function IdleWarning() {
 		alert("Warning, you will soon be logged out due to inactivity.");
 	}
 
-	// Logout the user.
+	//End user session
 	function IdleTimeout() {
 		window.location = logoutUrl;
 	}
